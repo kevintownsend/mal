42a29c5c6348f25ea1611ea2f0d2de70eebe7bcc
Kevin Townsend <kt@krtownsend.com>
2014-05-16 08:18:40 +0100

    .

--- matrixImage.py
+++ matrixImage.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import sys
+#TODO: scale to 2*median
 input()
 #M, N, nnz = all(
 M, N, nnz= (int(i) for i in input().split())
@@ -7,20 +9,27 @@ height = int(M/(N/1000.0))
 scale = N/1000.0
 freqImage = [[0 for i in range(width+1)] for j in range(height+1)]
 for i in range(nnz):
-    y, x, v = (int(j) for j in input().split())
+    y, x, v = (int(j)-1 for j in input().split())
     freqImage[int(y/scale)][int(x/scale)] = freqImage[int(y//scale)][int(x//scale)] + 1
 
 maxEl=0
+freqs=[]
 for i in range(height):
     for j in range(width):
+        if(freqImage[i][j] != 0):
+            freqs.append(freqImage[i][j])
         if(freqImage[i][j] > maxEl):
             maxEl=freqImage[i][j]
+
+freqs.sort()
+maxEl=min(freqs[len(freqs)*4//5],maxEl)
+sys.stderr.write("hello world")
 
 print("P2")
 print(str(width) + " "+str(height))
 print(maxEl)
 for i in range(height):
     for j in range(width):
-        print(str(maxEl-freqImage[i][j]) + " ", end="")
+        print(str(max(maxEl-freqImage[i][j],0)) + " ", end="")
     print()
 #M*N
